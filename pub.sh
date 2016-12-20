14054fe1cb5f614188c1e110de1ba8868ac0eb0e
John D <git16.x.jrd@xoxy.net>
2016-12-20 07:22:48 +0000

    ignore tmp chatter

--- pub.sh
+++ pub.sh
@@ -16,6 +16,8 @@ sed 's/img\//http\:\/\/blop.s3-us-west-2.amazonaws.com\/img\//g' src/$file | \
 sed 's/doc\//http\:\/\/blop.s3-us-west-2.amazonaws.com\/doc\//g' > tmp/$file
 done < "/home/ec2-user/cem/sources.txt"
 
+rm /home/ec2-user/cem/sources.txt
+
 cd tmp
 mv ref/* .
 ls -1 *.md | cut -f 1 -d '.' > sources.txt
@@ -25,12 +27,12 @@ docker run -v `pwd`:/source jagregory/pandoc -f markdown -t html-raw_html --stan
 sudo cp $file.html /var/www/html/
 done < "/home/ec2-user/cem/tmp/sources.txt"
 
+rm /home/ec2-user/cem/tmp/sources.txt
+
 cd ../src
 sudo cp emroseclub.php /var/www/html/
 sudo cp empix.txt /var/www/html/
 sudo cp link_list.txt /var/www/html/
+sudo cp index.html /var/www/html/
 
 cd ..
-# rm sources.txt
-
-
