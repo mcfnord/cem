d14e78178fe82759a50bf823d5310dd6ea7793f0
John D <git16.x.jrd@xoxy.net>
2016-12-30 06:02:49 +0000

    same features, cleaner code

--- src/emroseclub.php
+++ src/emroseclub.php
@@ -1,54 +1,45 @@
 <?php
-$file="empix.txt";
-$linecount = 0;
-$handle = fopen($file, "r");
-while(!feof($handle)){
-  $line = fgets($handle);
-  $linecount++;
+
+function numlines($filename)
+{
+  $linecount = 0;
+  $handle = fopen($filename, "r");
+  while(!feof($handle)){
+    $line = fgets($handle);
+    $linecount++;
+  }
+  fclose($handle);
+  return $linecount;
 }
 
-fclose($handle);
+function randoline($filename)
+{
+  $which = rand ( 0, numlines($filename) - 1 );
+  $file = new SplFileObject($filename);
+  $file->seek( $which );
+  echo $file->current() ; // read it from the file
+}
 
-# echo $linecount;
-
-$which = rand ( 0, $linecount - 1 );
-$file = new SplFileObject($file);
-$file->seek( $which );
 echo '<html><META HTTP-EQUIV="REFRESH" CONTENT="45"><title>Emily Rose Club</title><body bgcolor=black><center><font size="+2"><br></font>' ;
+echo '<br><font color="white"><i><b><a href="/">another</a></b></i><br>' ;
 
-echo '<br><font color="white"><i><b><a href="/">another</a></b></i><br>' ;	
-
-if (isset($_GET['my']))  // the my param is our source
-{ 
+if (isset($_GET['my']))   // the my param is our source
+{
 	echo "<img width='80%' src='" ;
-	echo urldecode( $_GET['my'] ) ; 
+	echo urldecode( $_GET['my'] ) ;
 	echo "'>" ;
-} 
+}
 else
 {
-	echo $file->current() ; // read it from the file
+  echo randoline("empix.txt");
 }
 
-$file="link_list.txt";
-$linecount = 0;
-$handle = fopen($file, "r");
-while(!feof($handle)){
-  $line = fgets($handle);
-  $linecount++;
+if( 0 == rand( 0, 3 ))    // flip a coin heads twice to see something I can read.
+{
+  echo "<br><b><a target='blank' href='" ;
+  echo trim(randoline("link_list.txt")) ;
+  echo "'>Read...</a></b>" ;
 }
 
-# flip a coin heads twice to see something I can read.
-$show = rand ( 0, 3 ); 
-if( 0 == $show )
-	{
-	$which = rand ( 0, $linecount - 1 );
-	$file = new SplFileObject($file);
-	$file->seek( $which );
-
-	echo "<br><b><a target='blank' href='" ;
-	echo rtrim($file->current()) ;
-	echo "'>Read...</a></b>" ;
-	}
-
-echo '</center></font></body>' ;	
+echo '</center></font></body>' ;
 ?>
