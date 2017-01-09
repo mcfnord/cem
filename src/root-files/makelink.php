9e6a9a0a2a19fb1ad4aa05d67f22f2980864f214
John D <git16.x.jrd@xoxy.net>
2017-01-09 22:33:07 +0000

    cleaner but totally broken. mkdir isn't working

--- src/root-files/makelink.php
+++ src/root-files/makelink.php
@@ -1,38 +1,31 @@
 <?php
 # call this from makelink.bat, which is in the path for this PC
 
-if(false === isset($_GET['cute_url']))
-	{
-	echo 'ERROR' ;
-	return ;
-	}
+if(false === isset($_GET['cute']))
+  { echo 'ERROR' ; return ; }
 
-if(false === isset($_GET['ugly_url']))
-	{
-	echo 'ERROR' ;
-	return ;
-	}
+if(false === isset($_GET['ugly']))
+  { echo 'ERROR' ; return ; }
 
-$local_dir = $_GET['cute_url'] ;
+$local_dir = $_GET['cute'] ;
 
-if(file_exists ( $local_dir ))  # does dest exist on this server?
-	{
-		echo 'ERROR' ;    # it exists, and that's an error!
-		return ;
-	}
-	else
-	{
-		mkdir( '_/' . $local_dir ) ;   # dir doesn't exist so we make it
+if(file_exists ( $local_dir ))  //  does dest exist on this server?
+  { echo 'ERROR' ;   return ; } // does exist? then fail.
+else
+  {
+  echo $local_dir ;
 
-		# and put the dest url redirector in it in its simplest form.
-		$payload = "<HTML><HEAD><META HTTP-EQUIV='REFRESH' CONTENT='1;URL=" . $_GET['ugly_url'] . "'></HEAD></HTML>" ;
+  echo mkdir( './_/' . $local_dir, 0777, true ) ;   # recursive ok
 
-		#create a default index.php file in that dir
-		$index_script = $local_dir . "/_/index.php" ;
-		file_put_contents($index_script, $payload);
+  // and put the dest url redirector in it in its simplest form.
+  $payload = "<HTML><HEAD><META HTTP-EQUIV='REFRESH' CONTENT='1;URL=" . $_GET['ugly'] . "'></HEAD></HTML>" ;
 
-		# and return the popular url
-		echo 'http://em-is.us/_/' . $local_dir ;
-		return ;
-	}
+  // create a default index.php file in that dir
+  $index_script = $local_dir . "/_/index.php" ;
+  file_put_contents($index_script, $payload);
+
+  // and return the popular url
+  echo 'http://em-is.us/_/' . $local_dir ;
+  return ;
+}
 ?>
