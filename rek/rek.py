36e2e4dee05d8e7e0e92d758911edd37057856c7
jrd <gh18.x.jrd@xoxy.net>
2018-12-08 07:01:53 +0000

    query the labels and the faces, and salt away the results

--- rek/rek.py
+++ rek/rek.py
@@ -27,20 +27,36 @@ def main():
         cur.execute("SELECT name from blobs where NOT(scanned) LIMIT 2;")
         rows = cur.fetchall()
         for row in rows:
-            blobname = "ocean-img.v3.tar/" + row[0]
+            faces = None
             labels = None
+            blobname = "ocean-img.v3.tar/" + row[0]
             try:
-                response = client.detect_labels(Image={'S3Object':{'Bucket':'p0p','Name':blobname}})
-                labels = response['Labels']
-                print("got labels");
+                responseFaces = client.detect_faces(Image={'S3Object':{'Bucket':'p0p','Name':blobname}})
+                faces = responseFaces['FaceDetails']
             except:
                 print("bad image format? some bad thing happened.")
 
-            if labels is not None:
-                updater = "UPDATE blobs SET scanned=1, labels='" + urllib.quote(str(response['Labels'])) + "' WHERE name = '" + row[0] + "';"
-            else:
-                updater = "UPDATE blobs SET scanned=1 WHERE name = '" + row[0] + "';"
+            try:
+                responseLabels = client.detect_labels(Image={'S3Object':{'Bucket':'p0p','Name':blobname}})
+                labels = responseLabels['Labels']
+            except:
+                print("bad image format? some bad thing happened.")
+
+            if labels is None:
+                labels = ""
+            if faces is None:
+                faces = ""
+
+            fmt = "update blobs set scanned=1, labels='{0}', faces='{1}' where name = '{2}';"
+            updater = fmt.format(urllib.quote(str(labels)), urllib.quote(str(faces)), row[0])
             cur.execute(updater)
+            print row[0]
+
+#            if labels is not None:
+#                updater = "UPDATE blobs SET scanned=1, labels='" + urllib.quote(str(response['Labels'])) + "' WHERE name = '" + row[0] + "';"
+#            else:
+#                updater = "UPDATE blobs SET scanned=1 WHERE name = '" + row[0] + "';"
+#            cur.execute(updater)
 #            print('Detected labels for ' + blobname)
 #            for label in response['Labels']:
 #                print (label['Name'] + ' : ' + str(label['Confidence']))
