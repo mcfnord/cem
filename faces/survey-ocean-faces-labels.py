b99429f380de6f6e4926a785d5135e0c1079929e
jrd <mcfnord@gmail.com>
2018-12-30 05:20:13 -0800

    doesn't write tho

--- faces/survey-ocean-faces-labels.py
+++ faces/survey-ocean-faces-labels.py
@@ -4,9 +4,6 @@ import sqlite3
 from sqlite3 import Error
 import boto3
 import urllib
-import urllib2
-from PIL import Image
-import requests
 import pprint
 import json
 
@@ -21,19 +18,19 @@ def create_connection(db_file):
 
 
 def main():
-    database = "/home/ec2-user/cem/faces/ocean-metadata.db"
+    database = "/t/ocean-metadata.db"
 
     client=boto3.client('rekognition')
 
     db = create_connection(database)
     cur = db.cursor()
-    cur.execute("SELECT name from blobs where NOT(scanned) ;")
+    cur.execute("SELECT name from blobs where NOT(scanned) LIMIT 2 ;")
     rows = cur.fetchall()
     for row in rows:
         faces = ""
         labels = ""
         blobname = "ocean-img.v3.tar/" + row[0]
-        print row[0],
+        print(row[0]),
         try:
             fullFacesResp = client.detect_faces(Image={'S3Object':{'Bucket':'p0p','Name':blobname}})
             faces = fullFacesResp['FaceDetails']
@@ -52,31 +49,9 @@ def main():
             print(" label query failed."),
 
         fmt = "update blobs set scanned=1, labels='{0}', faces='{1}' where name = '{2}';"
-        updater = fmt.format(urllib.quote(str(labels)), urllib.quote(str(faces)), row[0])
-        cur.execute(updater)
+        cur.execute(fmt.format(urllib.parse.quote(str(labels)), urllib.parse.quote(str(faces)), row[0]))
         print
-
-        # don't do this stuff, just collect data with this code
-#        url = 'http://p0p.s3-us-west-2.amazonaws.com/' + str(blobname)
-        # print url
-#        request = urllib2.Request(url)
-#        img = urllib2.urlopen(request).read()
-#        with open ('./tmp/' + row[0], 'w') as f: f.write(img)
-#        im = Image.open('./tmp/' + row[0])
-
-        # print im.size
-#        box = (100, 100, 400, 400)
-#        region = im.crop(box)
-#        region.save("./chf3.jpg", "JPEG") # save a box of the image
-
-#            if labels is not None:
-#                updater = "UPDATE blobs SET scanned=1, labels='" + urllib.quote(str(response['Labels'])) + "' WHERE name = '" + row[0] + "';"
-#            else:
-#                updater = "UPDATE blobs SET scanned=1 WHERE name = '" + row[0] + "';"
-#            cur.execute(updater)
-#            print('Detected labels for ' + blobname)
-#            for label in response['Labels']:
-#                print (label['Name'] + ' : ' + str(label['Confidence']))
+    db.close()
 
 if __name__ == '__main__':
     main()
