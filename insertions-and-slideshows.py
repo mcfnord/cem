1da3ee5b1d99602d674032f89d92b099055d9c6c
jbird <git.x.jrd@xoxy.net>
2019-12-01 01:07:06 +0000

    p0p to b00p

--- insertions-and-slideshows.py
+++ insertions-and-slideshows.py
@@ -4,37 +4,43 @@ import time
 from HTMLParser import HTMLParser
 
 gfImporting = False
-gstrClassname = None
+gstrDivFName = None
+gstrDivId = None
 gInsertSought = False
 
 def S3BinaryHostReplace( str ):
-    str = str.replace("(img/", "(http://p0p.s3-us-west-2.amazonaws.com/img/")
-    str = str.replace("(doc/", "(http://p0p.s3-us-west-2.amazonaws.com/doc/")
+    str = str.replace("(img/", "(http://b00p.s3-us-west-2.amazonaws.com/img/")
+    str = str.replace("(doc/", "(http://b00p.s3-us-west-2.amazonaws.com/doc/")
     return str ;
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         global gfImporting
-        global gstrClassname
+        global gstrDivId
         if tag == 'div': # is apparently lower-ized caseically speaking
             for attr in attrs:
                 if attr[0] == 'id':
-                    if attr[1] == gstrClassname: # name that class
+                    if attr[1] == gstrDivId: # name that div
                         gfImporting = True
+                        print("found: " + gstrDivId)
 
     def handle_endtag(self, tag):
         global gfImporting
+        global gInsertSought
         if tag == 'div': ## Div fails. case sensitive html!
             gfImporting = False
             gInsertSought = False
+#            print("div closed for " + gstrDivId)
 
     def handle_data(self, data):
         global gfImporting
         if gfImporting==True:
             mainoutfile.write(S3BinaryHostReplace(data)) # show contents when we're importing
+            print("imported some stuff")
 
 fname = sys.argv[1]                # arg1 is source pathspec
 fname = fname.replace("ref/", "")  # remove /ref source prefix, if found
+print("for source file: " + fname)
 fname = fname.replace(".md", "")   # remove .md suffix, so I can add S1, S2 to core fname before .md
 mainoutfile = open("tmp/" + fname + ".md", "w")
 slide = 1
@@ -52,18 +58,22 @@ for line in sys.stdin:
 
     if line.lower().startswith('insert:'):
         gInsertSought = True
+        print("an insert is sought")
         params = line[len('insert:'):].split()
 #        filename ='src/' +  params[0]
-        gstrClassname = params[1]                   # set this global
+        gstrDivFName = params[0]
+        gstrDivId = params[1]                   # set this global
         insertfilename = 'src/' + params[0]
         if False == os.path.isfile(insertfilename):
             insertfilename = 'src/ref/' + params[0] # sometimes i forget to prepend the ref/
+            print("want: " + insertfilename)
         if False == os.path.isfile(insertfilename):
             insertfilename = 'src/ref/_' + params[0] #refs have the underscore but i can omit it (should be sure there's no same name in root!)
         if False == os.path.isfile(insertfilename):
-            print("insert: source file not found, so ctrl-c and fix: " + insertfilename)
+            print("insert: source file not found, so ctrl-c and fix: " + params[0])
             time.sleep(99999)
             sys.exit() # won't stop this but worthy effort
+        print("found it, opening..." + insertfilename)
         f =  open(insertfilename, "r")
 	parser.feed(f.read())
         parser.close()
@@ -71,7 +81,7 @@ for line in sys.stdin:
         mainoutfile.write(S3BinaryHostReplace(line)),
 
 if True == gInsertSought:
-    print("insert id not found, so ctrl-c and fix. but this is broken")
-#    time.sleep(99999)
+    print("insert id not found, so ctrl-c and fix. fname:" + gstrDivFName + ", with div id: " + gstrDivId)
+    time.sleep(99999)
 
 mainoutfile.close()
